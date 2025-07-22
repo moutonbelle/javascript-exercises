5dbfebc04231b747c5ec71bc5267e4d71414c0ca
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-22 17:02:17 -0500

    Solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    for(let i = 0; i < num; i++) repeatedString += string;
 };
 
 // Do not edit below this line

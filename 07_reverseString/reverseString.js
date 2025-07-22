3b1b0b86c1fe72d7d9621eb59b47bc26ce1f54fa
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-22 17:13:59 -0500

    Solve 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let input = [...string];
+    let output = new Array(input.length).fill("");
+    let i, j;
+    for (i = 0, j = input.length; i < j; i++, j--) {
+        output[i] = input[j];
+        output[j] = input[i];
+    }
+    if (i === j) output[i] = input[i];
+    return output.join("");
 };
 
 // Do not edit below this line

44e99d9a49968033c583d2515c853b0a777babfb
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-22 18:01:14 -0500

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        for(let i = 0; i < arr.length; i++) {
+            if (arg === arr[i]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
 };
 
 // Do not edit below this line

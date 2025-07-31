ee61be89b8c32ba7f469f2d4798c7507cc166ddc
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-31 18:55:08 -0500

    Solve 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const regexIsLetter = /[a-zA-Z0-9]/;
+    let letters = str.split("").filter(char => regexIsLetter.test(char)).join("").toLowerCase();
+    let i = 0, j = letters.length-1;
+    while (i < j) {
+        if (letters[i] !== letters[j]) return false;
+        i++; j--;
+    }
+    return true;
 };
 
 // Do not edit below this line

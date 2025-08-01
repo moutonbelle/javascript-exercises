e26c803a12216fccca558cacc79d6e5ce47ae965
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-31 19:04:29 -0500

    Solve 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = Number(n);
+    if(num < 0) return "OOPS";
+    else if(num === 0) return 0;
+    else if(num === 1) return 1;
+    else return fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line

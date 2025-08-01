0b20cfa31496230612b10fd54308390a8cc4dd9c
Mouton Belle <219940491+moutonbelle@users.noreply.github.com>
2025-07-31 19:28:24 -0500

    Solve 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people
+        .map(person => {
+                person.age = (("yearOfDeath" in person) ? person.yearOfDeath : new Date().getFullYear()) - person.yearOfBirth;
+                return person;
+            })
+        .sort((a, b) => b.age - a.age)
+        [0];
 };
 
 // Do not edit below this line
